3fd6024bc1658ff9a441d32a66e02dc523c9dd4f
Mannu221 <alannkriss07@gmail.com>
2020-06-21 09:04:20 -0700

    Frustrating commit

---
+++ P.py
@@ -0,0 +1,25 @@
+#My details
+
+print("Name : "+"Somil"+" Swapnil"+" Chandra");
+print("College : "+"DIT"+" University"+" Dehradun")
+
+#Initialization
+
+maths=90;
+eng=88;
+hindi=90;
+chem=97;
+comp=95;
+
+#Calculating total
+
+total=maths+hindi+eng+chem+comp;
+
+#Calculating percentage
+
+p=total/5*100;
+
+#Displaying result
+
+print("Total Marks : ",total);
+print("Percentage : ",p);
